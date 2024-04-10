a34ea261c788d21037d2ee96b7e3ce6aa496dc33
LihanChen2004 <757003373@qq.com>
2024-04-11 02:06:48 +0800

    🎈 perf: 决策脚本添加预设参数

--- decision.sh
+++ decision.sh
@@ -1,6 +1,12 @@
 #!/bin/zsh
 
 # Start rm_decision
+
+    # attack_left
+    # attack_right
+    # retreat_attack_left
+    # protect_supply
+
 source install/setup.sh
 ros2 launch rm_behavior_tree rm_behavior_tree.launch.py \
     style:=retreat_attack_left \
